919c1c80f5a1f3e5bf3bba45984610d08256a394
sangamesh9036 <sangameshrb06@gmail.com>
2024-06-16 01:18:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello, Jenkins!'));
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://13.60.32.169:${port}`));
 
